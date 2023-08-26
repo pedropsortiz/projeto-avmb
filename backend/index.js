0a2d610a2b575b04ed8adcefad17f56a3a1df6ef
Pedro Ortiz <pedrops.ortiz@gmail.com>
2023-08-26 19:07:21 -0300

    Updating files

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
+const fs = require("fs");
+const path = require("path");
 const routes = require("./routes");
 
 const app = express();
@@ -8,12 +10,26 @@ const port = 8080;
 // Configuração do rate limiting: 2 requisições por minuto
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minuto
-  max: 50, 
+  max: 50,
   message: "Limite de requisições excedido. Por favor, tente novamente mais tarde."
 });
 
 // Aplicando o rate limiting a todas as rotas
 app.use(limiter);
+
+// Middleware para registrar requisições em um arquivo de log
+app.use((req, res, next) => {
+  const logFilePath = path.join(__dirname, "request_log.txt");
+  const logData = `${new Date().toISOString()} - IP: ${req.ip} - Method: ${req.method} - URL: ${req.originalUrl}\n`;
+
+  fs.appendFile(logFilePath, logData, (err) => {
+    if (err) {
+      console.error("Erro ao registrar requisição:", err);
+    }
+  });
+
+  next();
+});
 
 routes(app);
 
