3baf5a8cde3895c4bea528e2a5644d96b175e31b
Pedro Ortiz <pedrops.ortiz@gmail.com>
2023-08-26 10:57:40 -0300

    Updating files

---
+++ backend/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    pwd: {
+        type: String,
+        required: true
+    },
+    login: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    permission: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
