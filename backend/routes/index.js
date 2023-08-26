c56f2c4ee045add005c106ac539e5f194538ece6
Pedro Ortiz <pedrops.ortiz@gmail.com>
2023-08-26 19:40:21 -0300

    Updating files

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const bodyParser = require("body-parser"); const cors = require('cors')
-const produtosRoute = require("./produtosroutes.js");
-const usuariosRoute = require("./usuariosroutes.js");
-module.exports = (app) => {
-  app.use(cors());
-  app.use(bodyParser.json());
-app.use(produtosRoute);
-app.use(usuariosRoute);
-};
